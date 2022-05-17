b170ffd1a6a411079417a30163cde6f98fa04a1c
dslpz <dslpz728@gmail.com>
2022-05-17 12:24:58 -0500

    started script.js file

---
+++ script.js
@@ -0,0 +1,7 @@
+const timeEl = document.getElementById('time');
+const dateEl = document.getElementById('date');
+const currentWeatherItemsEl = document.getElementById('current-weather-items');
+const cityEl = document.getElementById('city');
+const stateEl = document.getElementById('state');
+const weatherForecastEl = document.getElementById('weather-forecast');
+const currentTempEl = document.getElementById('current-temp');
